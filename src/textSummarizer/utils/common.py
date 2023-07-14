ccfa55c2eb057d060fe4bfb9c97b4a1577115385
mhmamouch <98241045+MohamedHmamouch@users.noreply.github.com>
2023-07-14 23:17:44 +0200

    data_ingestion functions

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -1,47 +1,37 @@
 import os 
 from src.textSummarizer.logging import logger
-from ensure import ensure_annotations
+# from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
 import yaml
 from box.exceptions import BoxValueError
 
-@ensure_annotations
-def read_yaml(path_to_yaml:yaml)->ConfigBox:
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
 
-
-    """ Read yaml file
-
-    Args: 
-        path_to_yaml (str): path to yaml file
+    Args:
+        path_to_yaml (str): path like input
 
     Raises:
-        ValueError : if yaml file is empty
+        ValueError: if yaml file is empty
         e: empty file
 
-    Return:
-        ConfigBox: ConfigBox type    
+    Returns:
+        ConfigBox: ConfigBox type
     """
-
     try:
-
         with open(path_to_yaml) as yaml_file:
-            content=yaml.safe_load(yaml_file)
-
-            logger.info(f"yaml file {path_to_yaml} load successfully")
-
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
-
         raise ValueError("yaml file is empty")
-    
     except Exception as e:
         raise e
     
 
 
-@ensure_annotations
 def create_directories(path_to_directories:list, verbose=True):
 
 
@@ -60,7 +50,7 @@ def create_directories(path_to_directories:list, verbose=True):
 
                 logger.info(f"created directory at {path}")
 
-@ensure_annotations
+
 def get_size(path:Path)-> str:
      
      """ get size in KB
