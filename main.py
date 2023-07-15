aed2d6203f47e128a3487bd791e53e71c65817ec
mhmamouch <98241045+MohamedHmamouch@users.noreply.github.com>
2023-07-15 21:14:29 +0200

    end of data validation with test

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from src.textSummarizer.logging import logger
+from src.textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+
+
+
 
 STAGE_NAME="Data Ingestion Stage"
 
@@ -8,6 +12,21 @@ try:
     logger.info(f">>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
     data_ingestion=DataIngestionPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} ended <<<<<<<<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e 
+
+
+
+STAGE_NAME="Data Validatipn Stage"
+
+try:
+
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+    data_validation=DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>>>>>>> stage {STAGE_NAME} ended <<<<<<<<<")
 
 except Exception as e:
