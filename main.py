620d40445b6f8210419d8aad282a2a7636113a86
mhmamouch <98241045+MohamedHmamouch@users.noreply.github.com>
2023-07-15 14:53:35 +0200

    data ingestion test

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from src.textSummarizer.logging import logger
-logger.info('hello')
+
+STAGE_NAME="Data Ingestion Stage"
+
+try:
+
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+    data_ingestion=DataIngestionPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} ended <<<<<<<<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e 
